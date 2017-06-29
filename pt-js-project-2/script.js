d3008b664b3623241b4bda9c05103855ed0244ba
Kelly Knights <kknights@katalystadvantage.com>
2017-06-29 12:39:47 -0400

    initil build, continued

--- pt-js-project-2/script.js
+++ pt-js-project-2/script.js
@@ -1,91 +1,96 @@
-$(function() {
+var flashcards = {};
+var userAnswer;
+var flashcard_item = $('.flashcard');
+var flashcard_message = $('.flashcard_message');
+var correctMessage = "yay! thats correct" + "<br>";
+var incorrectMessage = "nope! thats incorrect" + "<br>";
+var successMessage = "!Felicidades! You've gotten everything correct! 🙌";
 
-  const flashcard_01 = $('.flashcard01');
-  const flashcard_02 = $('.flashcard02');
-  const flashcard_03 = $('.flashcard03');
+// todo: make the questions and answers in an array
+var correctAnswers = {
+  card_01: 'soporific, sleepy; numbing',
+  card_02: 'to stalk, to lie in wait for',
+  card_03: 'a grasshopper'
+};
 
-  // play sound on icon click
+
+// get the current card
+var activeCard = $('.is-active');
+var currentCard = $(activeCard[0]).data('card');
+
+
+// 0. get the user answer on input click,
+flashcards.getUserAnswer = function(){
+  $("input").on("click", function(){
+    userAnswer = $("input[type='radio']:checked").val();
+    flashcards.answerCheck();
+  });
+}
+
+// 1. check to see if the user answer matches the correct answer
+flashcards.answerCheck = function(){
+  if (userAnswer == correctAnswers[currentCard]) {
+    flashcards.rightAnwser();
+  } else {
+    flashcards.wrongAnswer();
+  }
+};
+
+// 2. if answer is correct, display message and load next card
+flashcards.rightAnwser = function(){
+  flashcard_message.append(correctMessage);
+  flashcards.loadNextFlashcard();
+};
+
+
+// 3. change is-active states and load next flashcard
+flashcards.loadNextFlashcard = function(){
+  setTimeout(function request() {
+    $(activeCard).removeClass('is-active').addClass('is-inactive');
+    $(activeCard).next('.flashcard').removeClass('is-inactive').addClass('is-active');
+
+    // grab the newly active card
+    activeCard = $('.is-active');
+    currentCard = $(activeCard[0]).data('card');
+    flashcards.answerCheck();
+
+    // remove the message
+    $(flashcard_item).find(flashcard_message).empty();
+  }, 2000);
+};
+
+
+// 4. if the user picks the wrong answer
+flashcards.wrongAnswer = function(){
+  flashcard_message.append(incorrectMessage);
+  $("input:checked").removeAttr("checked");
+};
+
+
+// SCRIPT INIT
+// ============================
+flashcards.init = function(){
+  flashcards.getUserAnswer();
+
   $('.play').on('click', function(){
     $(this).next('audio').trigger('play');
   });
 
-  // switch the language of the header onClick
-  function translateHeader(){
-    var translateHeaderIcon = $('.translate');
-    $(translateHeaderIcon).on('click', function(){
-      $('.flashcard_title_es').toggleClass('is-inactive');
-      $('.flashcard_title_en').toggleClass('is-active');
-    });
-  };
-  translateHeader();
+  var translateHeaderIcon = $('.translate');
+  $(translateHeaderIcon).on('click', function(){
+    $('.flashcard_title_es').toggleClass('is-inactive');
+    $('.flashcard_title_en').toggleClass('is-active');
+  });
 
 
-  // flashcard 01
-  const flashcard01 = function(){
+  $('#page-reload').click(function(e) {
+    e.preventDefault();
+    location.reload();
+  });
 
-    $("input").on( "click", function() {
-      var inputValue = $( "input:checked" ).val();
-
-      if( $("#option2:checked").length > 0){
-
-        $(this).next('label').addClass('is-correct');
-        $( ".flashcard_message" ).append("😸 Felicidades! '" + inputValue + "' es correcto!" + "<br>");
-        $("input:not(#option2)").attr('disabled',true).css('cursor', 'not-allowed');
-        $('.flashcard_next').addClass('is-active');
-
-        let loadNextFlashcard = setTimeout(function request() {
-          flashcard02();
-        }, 2000);
-
-      } else {
-        $( ".flashcard_message" ).append("😿 Lo siento, pero '" + inputValue + "' es incorrecto" + "<br>");
-        $(this).next('label').addClass('is-incorrect');
-        $("input:checked").removeAttr("checked");
-      }
-    })
-  };
-  flashcard01();
+};
 
 
-  // flashcard 2
-  const flashcard02 = function(){
-
-    $(flashcard_01).addClass('is-inactive');
-    $(flashcard_02).removeClass('is-inactive');
-
-    let conjugateEs = [{
-      yo: "acecho",
-      tu: "acechas",
-      él: "acecha",
-      ella: "acecha",
-      Usted: "acecha",
-      nosotros: "acechamos",
-      vosotros: "acecheis",
-      ellos: "acechan",
-      ellas: "acechan",
-      Ustedes: "acechan"
-    }];
-
-    let conjugateEn = [{
-      i: "stalk",
-      you: "stalk",
-      he: "stalks",
-      she: "stalks",
-      You: "stalk",
-      we: "stalk",
-      you: "stalk",
-      they: "stalk",
-      You: "stalk"
-    }];
-
-    console.log(conjugateEs);
-    console.log(conjugateEn);
-
-  };
-
-
-
-
-
-
+$(function() {
+  flashcards.init();
 });
